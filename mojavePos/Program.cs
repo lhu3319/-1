b49a0fc20c4c9bf503520469bb5133d34809c1ce
Han <lhu3319@nate.com>
2019-01-17 10:47:36 +0900

    456

--- mojavePos/Program.cs
+++ mojavePos/Program.cs
@@ -19,7 +19,7 @@ namespace mojavePos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManagerForm());
+            Application.Run(new FORM_01());
         }
     }
 }
